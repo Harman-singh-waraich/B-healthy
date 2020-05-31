bb5a96a944012ec7b8b1a66f9d7fd0026a120dce
Harmeet singh <WaraichHarman68@gmail.com>
2020-05-31 20:04:32 +0530

    contract written and frontend in development

---
+++ src/components/home.js
@@ -0,0 +1,124 @@
+import React from 'react'
+class Home extends React.Component{
+  render(){
+    return <div>
+    <section class="menu cid-s0z9wIHAuk" once="menu" id="menu1-0">
+
+
+
+<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<div class="hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+</button>
+<div class="menu-logo">
+<div class="navbar-brand">
+    <span class="navbar-logo">
+        <a href="https://mobirise.co">
+             <img src="assets/images/logo2.png" alt="Mobirise" style={{height: 3.8}}/>
+        </a>
+    </span>
+    <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="index.html">B-Healthy<br/></a></span>
+</div>
+</div>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+        <a class="nav-link link text-white display-4" href="index.html">
+            <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>Home</a>
+    </li><li class="nav-item"><a class="nav-link link text-white display-4" href="page1.html"><span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+            My Reports</a></li>
+    <li class="nav-item">
+        <a class="nav-link link text-white display-4" href="https://mobirise.co">
+            <span class="mbri-search mbr-iconfont mbr-iconfont-btn"></span>Search</a>
+    </li></ul>
+</div>
+</nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/z">best free website templates</a></section><section class="header15 cid-s0z9FUwkhE mbr-fullscreen mbr-parallax-background" id="header15-1">
+
+
+
+<div class="mbr-overlay" style={{opacity: 0.5, backgroundColor: "rgb(7, 59, 76)"}}></div>
+
+<div class="container align-right">
+<div class="row">
+<div class="mbr-white col-lg-8 col-md-7 content-container">
+    <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">B-Healthy</h1>
+    <p class="mbr-text pb-3 mbr-fonts-style display-5">As panic ensues due to the uprising of the novel coronavirus,Digital health records are relied upon to ensure authorised medical personnel can provide an optimum level of healthcare.<br/>On B-Healthy ,users can publish their health records safely on the blockchain and ensure that only authorised personnel can access them anywhere in the world.</p>
+</div>
+<div class="col-lg-4 col-md-5">
+    <div class="form-container">
+        <div class="media-container-column" data-form-type="formoid">
+
+            <form action="https://mobirise.com/" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form">
+                <div class="row">
+                    <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
+                    <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                    </div>
+                </div>
+                <div class="dragArea row">
+                    <div class="col-md-12 form-group " data-for="name">
+                        <input type="text" name="name" placeholder="Name" data-form-field="Name" required="required" class="form-control px-3 display-7" id="name-header15-1" />
+                    </div>
+
+
+                    <div data-for="message" class="col-md-12 form-group ">
+                        <textarea name="message" placeholder="upload file ,supported format: PDF" data-form-field="Message" class="form-control px-3 display-7" id="message-header15-1"></textarea>
+                    </div>
+                    <div class="col-md-12 input-group-btn">
+                        <button type="submit" class="btn btn-secondary btn-form display-4">SEND FORM</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+</section>
+
+<section once="footers" class="cid-s0zjQiOn5R mbr-reveal" id="footer7-8">
+
+
+
+
+
+<div class="container">
+<div class="media-container-row align-center mbr-white">
+<div class="row row-links">
+    <ul class="foot-menu">
+
+
+
+
+
+    <li class="foot-menu-item mbr-fonts-style display-7">
+            <a class="text-white mbr-bold" href="#" target="_blank">About us</a>
+        </li><li class="foot-menu-item mbr-fonts-style display-7">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</li><li class="foot-menu-item mbr-fonts-style display-7">
+            <a class="text-white mbr-bold" href="#" target="_blank">Get In Touch</a>
+        </li><li class="foot-menu-item mbr-fonts-style display-7">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</li><li class="foot-menu-item mbr-fonts-style display-7">
+            <a class="text-white mbr-bold" href="#" target="_blank">Work</a>
+        </li></ul>
+</div>
+<div class="row social-row">
+    <div class="social-list align-right pb-2">
+
+</div>
+</div>
+<div class="row row-copirayt">
+    <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+        © Copyright -Harmeet Singh</p>
+</div>
+</div>
+</div>
+</section>
+            </div>
+  }
+}
+export default Home;
--- src/contracts/b-health.sol
+++ src/contracts/b-health.sol
@@ -1,14 +1,49 @@
 pragma solidity^0.5.16;
 pragma experimental ABIEncoderV2;
+
 contract Health{
+
   string public name = "Health contract";
-  mapping(address => address[]) public UserAuthorisations;
-  mapping(address => string[])  public UserReports;
-  function getUserReports() public view returns(string [] memory){
-    return UserReports[msg.sender];
+  mapping(address => address[]) UserAuthorisations;
+  mapping(address => string[])  UserReports; // maintain reports belonging to a user
+  struct File {
+    string fileHash;
+    address User;
+    address [] Auths;
+
   }
+  mapping(string => File) Files;
+  event AuthAdded(
+    string _fileHash,
+    address _auth
+    );
+
+
+  function getUserReports(address _address) public view returns(string [] memory){
+    return UserReports[_address];
+  }
+
+  function getAuthsForReport(string memory _fileHash) public view returns(address [] memory ){
+    return Files[_fileHash].Auths;
+  }
+
+  function GetDetailedReport(string memory _fileHash) public returns(File memory){
+    return Files[_fileHash];
+  }
+  function addAuth(string memory _fileHash ,address _auth ) public {
+    File storage _file = Files[_fileHash];
+    require(_file.User == msg.sender, "Only user can add Authorisations!");
+
+    _file.Auths.push(_auth);
+    emit AuthAdded(_fileHash,_auth); //emit an event when new authorisation is given
+  }
+
   function SaveReport(string memory _fileHash) public returns(bool){
     UserReports[msg.sender].push(_fileHash);
+    File memory _file;
+    _file.fileHash = _fileHash;
+    _file.User = msg.sender;
+    Files[_fileHash] = _file;
     return true;
   }
 }
